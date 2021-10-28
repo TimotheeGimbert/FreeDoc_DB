684ffe385ca50f83942ca95d1713207b2f7c6da2
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-28 16:45:35 +0200

    seeds cities related

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,33 +7,47 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+Appointment.destroy_all
 Patient.destroy_all
 Doctor.destroy_all
-Appointment.destroy_all
+City.destroy_all
 
-50.times do
-  patient = Patient.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
-  )
-  puts patient
-end
+bdx = City.create(name: 'Bordeaux')
+nan = City.create(name: 'Nantes')
+par = City.create(name: 'Paris')
 
 10.times do
   doctor = Doctor.create(
     first_name: Faker::Name.first_name,
     last_name: 'Dr. ' + Faker::Name.last_name,
     specialty: ['generalist', 'globalist', 'limitist', 'placebist'][rand(0..3)],
-    zip_code: Faker::Number.within(range: 33000..34000)
+    zip_code: Faker::Number.within(range: 33000..34000),
+    city: [bdx, nan, par][rand(0..2)]
+
   )
   puts doctor
+end
+
+50.times do
+  patient = Patient.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    city: [bdx, nan, par][rand(0..2)]
+  )
+  puts patient
 end
 
 100.times do
   appointment = Appointment.create(
     doctor: Doctor.all[rand(0..9)],
-    patient: Patient.all[rand(0..99)],
-    date: Faker::Date.between(from: 2.days.ago, to: Date.today)
+    patient: Patient.all[rand(0..50)],
+    date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+    city: [bdx, nan, par][rand(0..2)]
   )
   puts appointment
+end
+
+Appointment.all.each do |rdv|
+  rdv.update(city: rdv.doctor.city)
+  puts "Appointment scheduled at the doctor's city : " + rdv.city.name
 end
