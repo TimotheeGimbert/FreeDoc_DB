07ba981942fdf44258cfa5e8fa3faff90be32c80
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-28 14:40:20 +0200

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Patient.destroy_all
+Doctor.destroy_all
+Appointment.destroy_all
+
+50.times do
+  patient = Patient.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+  puts patient
+end
+
+10.times do
+  doctor = Doctor.create(
+    first_name: Faker::Name.first_name,
+    last_name: 'Dr. ' + Faker::Name.last_name,
+    specialty: ['generalist', 'globalist', 'limitist', 'placebist'][rand(0..3)],
+    zip_code: Faker::Number.within(range: 33000..34000)
+  )
+  puts doctor
+end
+
+100.times do
+  appointment = Appointment.create(
+    doctor: Doctor.all[rand(0..9)],
+    patient: Patient.all[rand(0..99)],
+    date: Faker::Date.between(from: 2.days.ago, to: Date.today)
+  )
+  puts appointment
+end
