3d8532007534a6a516ac69b87aba9ea525ff2fea
TimotheeGimbert <timothee.gimbert@free.fr>
2021-10-28 14:03:26 +0200

    adds models

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_093605) do
+ActiveRecord::Schema.define(version: 2021_10_28_115744) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "patient_id"
+    t.integer "doctor_id"
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
